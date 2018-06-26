99b56d703de8555d193099a0c287ef917fbc5c40
KBClayton <clayton.kevinb@gmail.com>
2018-06-25 20:53:54 -0500

    first backend

---
+++ table-express.js
@@ -0,0 +1,57 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/'));
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/'))
+var tables = [
+    {
+      name: "yoda",
+      phone: 1252421,
+      email: "yoda@none.com",
+      id: "somestuff"
+    },
+];
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "home.html"));
+});
+app.get("/tables", function(req, res) {
+    res.sendFile(path.join(__dirname, "tables.html"));
+});
+app.get("/reserve", function(req, res) {
+    res.sendFile(path.join(__dirname, "reserve.html"));
+});
+
+app.get("/404", function(req, res) {
+    // res.send("Welcome to the Star Wars Page!")
+    res.sendFile(path.join(__dirname, "404.html"));
+});
+
+app.post("/api/tables", function(req, res) {
+var newres = req.body;
+console.log(newcharacter);
+tables.push(newres);
+
+res.json(newres);
+});
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
+
+app.get("/api/tables", function(req, res) {
+return res.json(tables);
+});

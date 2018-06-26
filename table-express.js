4966b85dffffc05059d5ecf1914e2082f3980fca
KBClayton <clayton.kevinb@gmail.com>
2018-06-25 22:29:35 -0500

    intermediate steps

--- table-express.js
+++ table-express.js
@@ -4,9 +4,6 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
 // Sets up the Express app to handle data parsing
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(express.static(__dirname + '/'));
 // Sets up the Express App
 // =============================================================
 var app = express();

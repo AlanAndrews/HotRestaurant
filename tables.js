6ad47a8b52efa494070e36344f1fc3ec888b443b
Paul Baker <p.baker1@yahoo.com>
2018-06-27 19:28:36 -0500

    tables.js functioning

--- tables.js
+++ tables.js
@@ -1,29 +1,20 @@
 $.get("/api/tables/", function(data) {
-console.log(data);
-for(var i=0; i<4; i++){
-    if (data[i]) {
-        $("#Tables").show();
-        var table=$("<div>");
-        table.text(data[i].id);
-        $("#Tables").append(table);
-    }
-    else {
-        $("#name").text("There are no reservations");
-        $("#Tables").hide();
-    }
-}
-if(data.length>4){
-    for(var i=4; i<data.length; i++){
-        if (data[i]) {
-            $("#Waitlist").show();
-            var table=$("<div>");
-            table.text(data[i].id);
-            $("#Waitlist").append(table);
-        }
-        else {
-            $("#name").text("There is no waitlist");
-            $("#Waitlist").hide();
-        }
-    }
-}
+    console.log(data)
+    $("#reservations").html("");
+    $("#waitList").html("");
+    var reservationListData = data.slice(0, 5);
+    var waitListData = data.slice(5);
+    for(var i=0; i<reservationListData.length; i++){
+        $("#reservations").append($("<div>").attr("class", "alert alert-secondary").attr("role", "alert").attr("id", "reservation" + i));
+        $("#reservation" + i).append($("<span>").attr("class", "badge badge-pill badge-primary").attr("id", "res" + i));
+        $("#res" + i).html(i + 1);
+        $("#reservation" + i).append(" | " + reservationListData[i].name)  
+    };
+    for(var i = 0; i < waitListData.length; i++){
+    
+        $("#waitList").append($("<div>").attr("class", "alert alert-secondary").attr("role", "alert").attr("id", "waitList" + i));
+        $("#waitList" + i).append($("<span>").attr("class", "badge badge-pill badge-primary").attr("id", "wl" + i));
+        $("#wl" + i).html(i + 1);
+        $("#waitList" + i).append(" | " + waitListData[i].name)  
+    };
 });
